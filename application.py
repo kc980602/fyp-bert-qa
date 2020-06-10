5a22f455fc91dbd672e5baf88fb6ee5800a6c506
Kevin Cheung <cheung.puilam15@gmail.com>
2020-06-11 01:15:47 +0800

    req

--- application.py
+++ application.py
@@ -75,5 +75,22 @@ def get_answer(qa_id):
         return jsonify({"success": False})
 
 
-if __name__ == '__main__':
+# print a nice greeting.
+def say_hello(username="World"):
+    return '<p>Hello %s!</p>\n' % username
+
+
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
+
+app.add_url_rule('/', 'index', (lambda: header_text + say_hello() + instructions + footer_text))
+
+if __name__ == "__main__":
     app.run()
